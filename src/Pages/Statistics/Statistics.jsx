8408bcc3591b6357b7d44253b9a6f75209b09764
Mazharul-Islam-2046 <mdmazharulislam2046@gmail.com>
2023-09-26 21:53:02 +0600

    Statistics Page Chart Setup

--- src/Pages/Statistics/Statistics.jsx
+++ src/Pages/Statistics/Statistics.jsx
@@ -1,7 +1,48 @@
+import { useEffect, useState } from "react";
+import { Pie, PieChart, Tooltip, Cell, Legend} from "recharts";
+import { getStoredDonationInfo } from "../../Utility/StoredDonationsInfo";
+import { useLoaderData } from "react-router-dom";
+
+
+
 const Statistics = () => {
+    const [dataOfDonation, setDataOfDonation] = useState([]);
+    const donationFeilds = useLoaderData();
+
+useEffect(()=>{
+    const donatedFeilds = getStoredDonationInfo();
+    const donatedFeildsLength = donatedFeilds.length;
+    const donationFeildsLength = donationFeilds.length;
+    const donationLeft = donationFeildsLength - donatedFeildsLength;
+    const postiveDonationLeft = Math.abs(donationLeft)
+
+    const donationAmount = [
+        {type:"Donated", value: donatedFeilds.length},
+        {type:"DonationLeft", value: postiveDonationLeft}
+    ]
+    setDataOfDonation(donationAmount)
+},[donationFeilds])
+
+const COLORS = ['#FF444A', '#00C49F'];
     return (
-        <div>
-            <h1>Statistics</h1>
+        <div className="w-full flex justify-center items-center">
+            <PieChart width={500} height={500}>
+        <Pie
+        dataKey="value"
+        isAnimationActive={false}
+        data={dataOfDonation}
+        cx="50%"
+        cy="50%"
+        outerRadius={200}
+        fill="#8884d8"
+        label>
+            {dataOfDonation.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+            ))}
+        </Pie>
+        <Tooltip></Tooltip>
+        <Legend></Legend>
+            </PieChart>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -30,7 +30,8 @@ const router = createBrowserRouter([
       },
       {
         path: '/statistics',
-        element: <Statistics></Statistics>
+        element: <Statistics></Statistics>,
+        loader: () => fetch('/public/data.json')
       },
       {
         path: '/donation/:id',
