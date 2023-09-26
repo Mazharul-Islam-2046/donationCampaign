5638f053755991e9c1e2b6a5d53e7a18b1f36789
Mazharul-Islam-2046 <mdmazharulislam2046@gmail.com>
2023-09-27 05:00:23 +0600

    Previously my search function was onKeyup but in requirment video it's onsubmit that's why I changed it onsubmit

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
 
     const handleSearch = e => {
         e.preventDefault();
-        const searchText = e.target.value;
+        const searchText = e.target.searchBox.value;
         const searchReasultsArray = searchByCategories(searchText, donationFields);
         setDisplayData(searchReasultsArray);
     }
@@ -20,9 +20,11 @@ const Home = () => {
             <div className="flex flex-col gap-6 md:gap-10 justify-center items-center h-[60vh] relative overflow-hidden">
                 <h1 className="text-2xl text-center md:text-3xl lg:text-5xl font-bold">I Grow By Helping People In Need</h1>
                 <div>
-                    <input onKeyUp={handleSearch} placeholder="Search Here" type="text" className="w-64 px-4
+                    <form onSubmit={handleSearch}>
+                    <input name="searchBox" placeholder="Search Here" type="text" className="w-64 px-4
                     border py-2 border-r-0 rounded-l-md text-xs"/>
                     <input type="submit" value="Search" className="cursor-pointer border py-1 px-4 bg-red-500 text-white rounded-r-md" />
+                    </form>
                 </div>
                 <img src="https://i.ibb.co/qpPGqZC/fc1f79e18cdc1a12320b9b10ec3e253d.jpg" alt="" className="absolute z-[-10] opacity-5 " />
             </div>
